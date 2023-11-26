f988da5b82f04beca61a6d060343e98920eb9fa8
marcosnms <marcosnms@gmail.com>
2023-11-26 18:33:32 -0300

    simplificação para testes

--- src/api/controllers/userController.js
+++ src/api/controllers/userController.js
@@ -2,22 +2,6 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 exports.teste = async (req, res) => {
-  return res.status(200).json({ message: 'Server is running' });
+  console.log('Teste server');
+  return res.status(200).json({ message: 'Hello World' });
 };
-
-exports.createUserAccount = async (req, res) => {
-  const { name, whatsappNumber } = req.body;
-  try {
-    const newUser = await prisma.user.create({
-      data: {
-        name,
-        whatsappNumber,
-        privateKey: process.env.JWT_SECRET,
-      },
-    });
-    return res.status(201).json(newUser);
-  } catch (error) {
-    return res.status(500).json({ error: error.message });
-  }
-};
-
--- src/api/routes/userRoutes.js
+++ src/api/routes/userRoutes.js
@@ -5,21 +5,8 @@ const authMiddleware = require('../middlewares/authMiddleware'); // Importa o mi
 const router = express.Router();
 
 // Teste server
-router.post('/', userController.teste); // Teste server
+router.post('/teste', userController.teste); // Teste server
 
 // Chat 
-
-
-
-// Onboarding e Gerenciamento de Conta
-router.post('/register', userController.createUserAccount); // Cria uma nova conta de usuário
-router.post('/login', userController.loginUser); // Autentica um usuário
-router.get('/verify/:token', userController.verifyAccount); // Verifica a conta de usuário
-
-// Perfil de Usuário
-router.get('/profile/:userId', authMiddleware.verifyToken, userController.getUserProfile); // Obtém o perfil do usuário
-router.put('/profile/:userId', authMiddleware.verifyToken, userController.updateUserProfile); // Atualiza o perfil do usuário
-router.post('/profile/picture', authMiddleware.verifyToken, userController.uploadProfilePicture); // Carrega uma foto de perfil
-
 
 module.exports = router;
